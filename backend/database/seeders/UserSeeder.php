cb70db3f185b5a6b766b37c8293d2786c59951e1
vinicius-rech <vinicius.rech.dev@gmail.com>
2025-09-19 00:16:19 -0300

    feat: integrate Laravel Sanctum for API authentication and add user seeder

---
+++ backend/database/seeders/UserSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+            ['name' => 'Carlos', 'email' => 'carlos@example.com'],
+            ['name' => 'Fernanda', 'email' => 'fernanda@example.com'],
+            ['name' => 'Isabela', 'email' => 'isabela@example.com'],
+            ['name' => 'Rafael', 'email' => 'rafael@example.com'],
+            ['name' => 'Beatriz', 'email' => 'beatriz@example.com'],
+            ['name' => 'Rodrigo', 'email' => 'rodrigo@example.com'],
+        ];
+
+        foreach ($users as $user) {
+            User::create([
+                'name' => $user['name'],
+                'email' => $user['email'],
+                'password' => Hash::make('Password@123'),
+                'remember_token' => Str::random(10),
+                'email_verified_at' => now(),
+            ]);
+        }
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,3 +4,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
 Route::post('/login', [AuthController::class, 'login']);
+
+
+// Route::middleware('auth:sanctum')->group(function () {
+//     Route::get('/user', function (Request $request) {
+//         return $request->user();
+//     });
+// });
+
